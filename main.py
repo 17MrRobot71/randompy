ce3490fb8238c9ba3199bc74f7eb0c1c183cd4fe
17MrRobot71 <bosh0705@gmail.com>
2024-02-12 22:24:54 +0200

    new branch

--- main.py
+++ main.py
@@ -13,4 +13,11 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def kill(arg) :
+    print("Kill the " + arg)
+
+kill("John")
+kill("John")
+kill("John")
+kill("John")
+kill("John")
